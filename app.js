d1a713ef1665459fd540f272957dc62c2fa9bfaa
Rabeea Wajeeha <rwajeeha@MRK-01863.local>
2018-10-02 11:59:15 -0400

    icbt dashboard

---
+++ app.js
@@ -0,0 +1,22 @@
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Effort", "Amount given"],
+    ["My all", 75]
+  ]);
+
+  var options = {
+    pieHole: 0.5,
+    pieSliceTextStyle: {
+      color: "black"
+    },
+    legend: "none"
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById("donut_single")
+  );
+  chart.draw(data, options);
+}
